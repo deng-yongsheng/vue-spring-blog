379695f726e6e429b86d9105cc8980ab2d4250da
邓永盛 <1596953204@qq.com>
2021-12-28 16:54:33 +0800

    完善两个model

--- src/main/java/edu/hue/jk/models/Article.java
+++ src/main/java/edu/hue/jk/models/Article.java
@@ -2,7 +2,14 @@ package edu.hue.jk.models;
 
 import lombok.Data;
 
+import java.sql.Date;
+
 @Data
 public class Article {
+    Integer id;
+    String uuid;
+    String title;
+    Date datetime;
 
+    User Author;
 }
--- src/main/java/edu/hue/jk/models/User.java
+++ src/main/java/edu/hue/jk/models/User.java
@@ -2,7 +2,14 @@ package edu.hue.jk.models;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class User {
+    Integer id;
+    String username;
+    String password;
+    String usertype;
 
+    List<Article> articles;
 }
