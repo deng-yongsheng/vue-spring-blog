25feb0a2f1505cf767a0b2ceed9d8592aa66977d
Grace <9676155+graceonlyone@user.noreply.gitee.com>
2021-12-29 16:58:16 +0800

    添加文件

---
+++ src/main/java/edu/hue/jk/controllers/ArticleController.java
@@ -0,0 +1,9 @@
+package edu.hue.jk.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ArticleController {
+}
