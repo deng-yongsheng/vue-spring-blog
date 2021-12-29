ba69b9c58cb76b69c0b49d8fe626a1dbf483ecf2
sheng <1596953204@qq.com>
2021-12-29 20:57:28 +0800

    格式化代码

--- src/main/java/edu/hue/jk/controllers/UserController.java
+++ src/main/java/edu/hue/jk/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 @RequestMapping("/api")
 public class UserController {
