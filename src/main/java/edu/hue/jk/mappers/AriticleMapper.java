faa590a998800598855029807893332e15e9960f
Grace <9676155+graceonlyone@user.noreply.gitee.com>
2021-12-29 11:10:46 +0800

    添加一个功能

--- src/main/java/edu/hue/jk/mappers/AriticleMapper.java
+++ src/main/java/edu/hue/jk/mappers/AriticleMapper.java
@@ -1,11 +1,16 @@
 package edu.hue.jk.mappers;
 
+import edu.hue.jk.models.Article;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
-public class AriticleMapper {
+public interface AriticleMapper {
 
     // todo
     // 查询文章，上传文章，删除文章，获取某个用户的所有文章
-
+    @Select("select * from article where userid =#{userid}")
+    List<Article> getArticleListOfUser(Integer userid);
 }
