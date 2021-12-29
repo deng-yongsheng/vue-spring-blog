4a840ef4615c65588844f472d899fb1757d5562c
Grace <9676155+graceonlyone@user.noreply.gitee.com>
2021-12-29 10:43:35 +0800

    增加用户注册mapper

--- src/main/java/edu/hue/jk/mappers/UserMapper.java
+++ src/main/java/edu/hue/jk/mappers/UserMapper.java
@@ -1,6 +1,7 @@
 package edu.hue.jk.mappers;
 
 import edu.hue.jk.models.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -10,5 +11,7 @@ public interface UserMapper {
     @Select("select * from user where username=#{user} and password=#{password}")
     User login(String user,String password);
 
+    @Insert("insert into user(username,password) values(#{username},#{password})")
+    int rigister(User user);
 
 }
