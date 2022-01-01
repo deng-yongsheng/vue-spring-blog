8029d30fad59b2a5381c4908135266cd1f4af3d5
sheng <1596953204@qq.com>
2022-01-01 16:24:27 +0800

    登录状态管理和重定向

---
+++ src/main/resources/resources/js/token.js
@@ -0,0 +1,33 @@
+function hasToken() {
+	return localStorage.getItem('username') !== null;
+}
+
+function clearToken() {
+	return localStorage.clear();
+}
+
+function setToken(username) {
+	return localStorage.setItem('username', username);
+}
+
+function getToken() {
+	if (hasToken()) {
+		return localStorage.getItem('username');
+	} else {
+		throw new Error('没有token');
+	}
+}
+
+function setRedirect(url) {
+	sessionStorage.setItem('RedirectUrl', url);
+}
+
+function redirect(defaultUrl) {
+	var lastUrl = sessionStorage.getItem('RedirectUrl');
+	if (lastUrl !== null) {
+		sessionStorage.removeItem('RedirectUrl')
+		location.href = lastUrl;
+	} else {
+		location.href = defaultUrl;
+	}
+}
