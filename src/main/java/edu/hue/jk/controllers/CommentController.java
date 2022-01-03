557ed343212d3a1568a38cf7c4c8ff507a70f725
sheng <1596953204@qq.com>
2022-01-03 18:43:42 +0800

    统一函数注释格式

--- src/main/java/edu/hue/jk/controllers/CommentController.java
+++ src/main/java/edu/hue/jk/controllers/CommentController.java
@@ -3,11 +3,13 @@ package edu.hue.jk.controllers;
 import edu.hue.jk.mappers.CommentMapper;
 import edu.hue.jk.models.Comment;
 import edu.hue.jk.models.User;
+import edu.hue.jk.utils.IpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -16,40 +18,56 @@ public class CommentController {
     @Autowired
     private CommentMapper commentMapper;
 
-    //查询模块
-    /*
-    功能：根据文章编号查询相关的评论信息（测试成功）
+    /**
+     * 根据文章编号查询相关的评论信息（测试成功）
+     *
+     * @param articleid
+     * @return
      */
     @GetMapping("/searchAllComByArticleId")
     @ResponseBody
-    public String searchAllComByArticleId(Model model, String articleid) {
+    public String searchAllComByArticleId(@RequestParam String articleid) {
         List<Comment> commentList = commentMapper.getCommnentListByArticleId(articleid);
         return commentList.toString();
     }
 
-    /*
-    功能：根据文章编号和用户编号查询某文章下对应用户的所有评论信息（测试成功）
+    /**
+     * 根据文章编号和用户编号查询某文章下对应用户的所有评论信息（测试成功）
+     *
+     * @param articleid
+     * @param userid
+     * @return
      */
     @GetMapping("/searchAllCommmentByAIdandUId")
     @ResponseBody
-    public String searchAllCommmentByAIdandUId(Model model, String articleid, Integer userid) {
+    public String searchAllCommmentByAIdandUId(@RequestParam String articleid, @RequestParam Integer userid) {
         List<Comment> commentList = commentMapper.getCommentListByAIdandUId(articleid, userid);
         return commentList.toString();
     }
 
-    /*
-    功能：根据评论编号、文章编号以及用户编号查询某文章下对应用户的评论（测试成功）
+    /**
+     * 根据评论编号、文章编号以及用户编号查询某文章下对应用户的评论（测试成功）
+     *
+     * @param articleid
+     * @param userid
+     * @param id
+     * @return
      */
     @GetMapping("/searchUniqueComment")
     @ResponseBody
-    public String searchUniqueComment(Model model, String articleid, Integer userid, Integer id) {
+    public String searchUniqueComment(@RequestParam String articleid, @RequestParam Integer userid, Integer id) {
         Comment commentList = commentMapper.getUniqueComment(articleid, userid, id);
         return commentList.toString();
     }
 
-    //添加评论模块
-    /*
-    功能：插入一条用户评论记录（测试成功）
+    /**
+     * 添加一条评论
+     *
+     * @param request   用户请求,用来提取IP地址
+     * @param articleid 用户评论的文章id
+     * @param content   评论内容
+     * @param userid    用户id
+     * @return
      */
     @PostMapping("/addcomment")
     @ResponseBody
@@ -63,9 +81,12 @@ public class CommentController {
         }
     }
 
-    //删除评论模块
-    /*
-    功能：根据评论编号和文章编号删除一条评论（测试成功）
+    /**
+     * 根据评论编号和文章编号删除一条评论（测试成功）
+     *
+     * @param articleid
+     * @param id
+     * @return
      */
     @GetMapping("/delCommentByIdandAId")
     @ResponseBody
