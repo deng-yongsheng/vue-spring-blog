962a09ede23bfb81189a3573879df0f165621a3a
Grace <9676155+graceonlyone@user.noreply.gitee.com>
2022-01-03 10:32:04 +0800

    上传博客文章

--- src/main/java/edu/hue/jk/controllers/DocArticleController.java
+++ src/main/java/edu/hue/jk/controllers/DocArticleController.java
@@ -1,15 +1,13 @@
 package edu.hue.jk.controllers;
 
 import edu.hue.jk.mappers.DocArticleMapper;
+import edu.hue.jk.mappers.UserMapper;
 import edu.hue.jk.models.DocArticle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -18,6 +16,8 @@ public class DocArticleController {
 
     @Autowired
     private MongoOperations mongoOperations;
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * 根据uuid查询一篇文章
@@ -32,4 +32,18 @@ public class DocArticleController {
         model.addAttribute("article", article);
         return "article.html";
     }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(Model model, DocArticle article, @RequestHeader(value = "token", required = true) String username) {
+        if (userMapper.getUserByName(username)!=null){
+            mongoOperations.save(article, "article");
+            return "上传成功";
+        }else{
+            return "没有上传权限，请检查是否已经登录";
+        }
+
+    }
+
+
 }
