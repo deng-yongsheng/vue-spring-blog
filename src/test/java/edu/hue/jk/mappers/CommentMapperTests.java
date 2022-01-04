5ac8e651a09cf7be905da5ad614da8a29816b25d
Grace <9676155+graceonlyone@user.noreply.gitee.com>
2022-01-04 16:36:39 +0800

    修改CommentMapperTests增加测试

--- src/test/java/edu/hue/jk/mappers/CommentMapperTests.java
+++ src/test/java/edu/hue/jk/mappers/CommentMapperTests.java
@@ -25,6 +25,38 @@ public class CommentMapperTests {
      */
     @Test
     public void getCommentList() throws JsonProcessingException {
-        System.out.println(objectMapper.writeValueAsString(commentMapper.getCommnentListByArticleId("61d29e957921802b08fe28b6")));
+        System.out.println(objectMapper.writeValueAsString(commentMapper.getCommnentListByArticleId("61d2a0a97921802b08fe28b7")));
+    }
+    /**
+     * 根据文章编号和用户编号查询某文章下对应用户的所有评论信息
+     * 功能测试
+     */
+    @Test
+    public void getCommentByAandU() throws JsonProcessingException{
+        System.out.println(objectMapper.writeValueAsString(commentMapper.getCommentListByAIdandUId("61d2a0a97921802b08fe28b7", 2)));
+    }
+    /**
+     * 根据评论编号、文章编号以及用户编号查询某文章下对应用户的唯一评论
+     * 功能测试
+     */
+    @Test
+    public void getuniquecomment() {
+        System.out.println(commentMapper.getUniqueComment("61d2a0a97921802b08fe28b7", 2, 18));
+    }
+    /**
+     * 插入一条用户评论记录
+     * 功能测试
+     */
+    @Test
+    public void insertcomment(){
+        System.out.println(commentMapper.add("0:0:0:0:0:0:0:1", 2, "61d2a0a97921802b08fe28b7", "好啊好啊"));
+    }
+    /**
+     * 根据评论编号和文章编号删除一条评论
+     * 功能测试
+     */
+    @Test
+    public void delcomment(){
+        System.out.println(commentMapper.del("61d2a0a97921802b08fe28b7", 19));
     }
 }
